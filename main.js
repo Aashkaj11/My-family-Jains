4112cab6c4152a4476564b34671c0aa3914034e3
Aashkaj11 <75491227+Aashkaj11@users.noreply.github.com>
2021-03-18 19:56:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var discription=[
+    "My dadu is Mr.Varinder Kumar Jain.He is hardworking and kept the family together.",
+    "She is my grandmother.Neena Jain is the grandmom of 21st Cen.",
+    "My  papa Mr.Sachin Jain.He is very hardworking,constructive ,helpful and resourceful.He is a great dad,son ,brother and husband",
+    "My mumma is Dr.Shweta Jain.She is creative,hardworking,great cook as well as the best mom in the shole wide world.",
+    "My Chachi Ca.Bhavisha Jain.She is  hardworking,creative,academician and dancer and much more.",
+    "My Chachu is Ca.Kunal Jain.He is talented,hardworking ,adventurous and extremely naughty.",
+    "I am Aashka Jain.I am a good artist ,dancer and coder.I am talented ,passionate,hardworking ,creative,helpful and caring.",
+    "My  sister Ms.Aahana Jain AKA Khanoo Jain. She is intelligent,pasionate,talented,fashionista ,techosavi and cute as well as extremely naaughty.",
+    "My cousin sister Ms.Sia Jain AKA Gappu .She exteremly Cute ,Inquisitive ,Helpful,careful,naughty",
+    "I love My FAMILy!!!!!!!!!!!!!!!!!!"
+    ];
+    var images=[
+      "dadu.JPG","dadimaa.JPG","PAPA.JPG","cv pic.jpg","chachi.JPG","chchu.JPG","Aashka.JPG","Aahana.JPG","Sia.JPG","(((.jpg"
+    ];
+    var i=0;
+    function next_slide1(){
+    document.getElementById("1")
+    .innerHTML=discription[i];
+    document.getElementById("img1").src=images[i];
+    i++;
+    document.getElementById=("audio").play();
+    }
+    
